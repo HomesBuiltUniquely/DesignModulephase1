cb00890f9cd995af91f692bffb746a35d1a9711b
Vishal Kholakaiya <vishal@hubinterior.com>
2026-05-12 17:04:13 +0530

    resolve the error

--- my-app/app/SalesClosure/page.tsx
+++ my-app/app/SalesClosure/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import SalesClosureForm from "./Components/SalesClosureForm";
 import Header from "./Components/Header";
 
@@ -5,7 +6,9 @@ export default function SalesCloser() {
   return (
     <div>
       <Header />
-      <SalesClosureForm />
+      <Suspense fallback={<div className="min-h-screen bg-purple-50 flex items-center justify-center text-green-950">Loading form…</div>}>
+        <SalesClosureForm />
+      </Suspense>
     </div>
   );
 }
