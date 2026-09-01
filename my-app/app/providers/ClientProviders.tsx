6db63cd16e80c82e64df9c5a6a1b81f009546077
Vishal Kholakaiya <vishal@hubinterior.com>
2026-09-01 16:59:39 +0530

    removed un used files

--- my-app/app/providers/ClientProviders.tsx
+++ my-app/app/providers/ClientProviders.tsx
@@ -3,15 +3,12 @@
 import type { ReactNode } from 'react';
 import { AuthProvider } from '../auth/AuthContext';
 import { DesignNotificationProvider } from '../Components/DesignNotificationProvider';
-import P2pCelebrationOverlay from '../Components/P2pCelebrationOverlay';
 
 export function ClientProviders({ children }: { children: ReactNode }) {
   return (
     <AuthProvider>
-      <DesignNotificationProvider>
-        {children}
-        <P2pCelebrationOverlay />
-      </DesignNotificationProvider>
+      <DesignNotificationProvider>{children}</DesignNotificationProvider>
     </AuthProvider>
   );
 }
+ 
